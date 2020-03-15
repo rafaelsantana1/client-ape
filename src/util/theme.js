14a11f7609ce1aa91ef0e9a65a07a2cca512104e
santana <santana@santanas-MacBook-Pro.local>
2020-03-15 01:39:54 -0300

    Redux Setup Complete

---
+++ src/util/theme.js
@@ -0,0 +1,44 @@
+export default {
+    palette:{
+      primary:{
+        //     light: '#111111',
+        main: '#3344cc'
+        //     dark: '#111111',
+        //     contrastText: '#ffffff'
+      },
+      secondary: {
+        main: '#1122ee'
+  
+      }
+    },
+    spreadIt: {
+      typography:{
+        useNextVariants: true
+      },
+      form:{
+        textAlign: 'center'
+      },
+      image:{
+          margin: '20px auto 20px auto',
+          height: '160px'
+      },
+      pageTitle:{
+          margin: '10px auto 10px auto'
+      },
+      textField: {
+          margin: '10px auto 10px auto'
+      },
+      button: {
+          margin: 10,
+          position: 'relative'
+      },
+      customError:{
+          color: 'red',
+          fontSize: '0.8rem',
+          marginTop: '10px'
+      },
+      progress:{
+          position: 'absolute'
+      }
+    }
+  }
